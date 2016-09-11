527ae661642402c1b0d94f65f0b652106b2f9137
Wataru.Kasahara <kasahara1019@gmail.com>
2016-09-11 14:47:44 +0900

    Create the action and reducer for TODO

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import { addTodo } from './actions'
+import todo from './reducers'
 import App from './components/App'
 
+let store = createStore(todo)
+
+store.dispatch(addTodo('Add todo : Hello, world'))
+console.log(store.getState())
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
